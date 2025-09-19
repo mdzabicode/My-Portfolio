c1fd53880221b30cc4da80d262db25c06a10db87
md zabi khan <123319811+mdzabicode@users.noreply.github.com>
2025-09-19 16:50:30 +0530

    File name is fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import Skills from "./components/Skills";
-import Projects from "./components/Projects";
-import Freelance from "./components/Freelance";
-import Footer from "./components/Footer";
+import Navbar from "./Components/Navbar.jsx";
+import Hero from "./Components/Hero.jsx";
+import Skills from "./Components/Skills.jsx";
+import Projects from "./Components/Projects.jsx";
+import Freelance from "./Components/Freelance.jsx";
+import Footer from "./Components/Footer.jsx";
 
 export default function App() {
   return (
